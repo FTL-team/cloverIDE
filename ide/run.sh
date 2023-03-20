6c226fabe60a73420d6aa9133e602377d5a79bf9
max <mrom06@ya.ru>
2023-03-20 21:16:34 +0300

    Fixes

--- ide/run.sh
+++ ide/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+source /etc/profile
+source ~/.bashrc
 cd "$(dirname "$0")"
 THEIA_WEBVIEW_EXTERNAL_ENDPOINT="{{hostname}}" ./node ./src-gen/backend/main.js --plugins=local-dir:plugins -h 0.0.0.0 -p 7778
